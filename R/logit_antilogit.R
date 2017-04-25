7940135a91a657ceeba92f727aa268828aea20cb
liometopum <tim.szewczyk@gmail.com>
2017-04-25 17:24:51 -0600

    added logit & antilogit functions

---
+++ R/logit_antilogit.R
@@ -0,0 +1,33 @@
+
+#' logit function
+#'
+#' This function calculates the logit for the object 'p' (range 0 to 1),
+#' returning the corresponding values (range -Inf to Inf).
+#' @param p A numeric vector, matrix, or array of proportions
+#' @keywords logit, proportion
+#' @export
+#' @examples
+#' p <- runif(20, 0, 1)
+#' logit(p)
+
+logit <- function(p) {
+  log(p/(1-p))
+}
+
+
+
+
+#' antilogit function
+#'
+#' This function calculates the antilogit for the object 'x' (range -Inf to Inf),
+#' returning the corresponding proportions (range 0 to 1).
+#' @param x A numeric vector, matrix, or array
+#' @keywords antilogit, proportion
+#' @export
+#' @examples
+#' x <- runif(20, -100, 100)
+#' antlogit(x)
+
+antilogit <- function(x) {
+  exp(x)/(1+exp(x))
+}
